357dec8290185a59ec3f60138792fef6bf2d75d8
Nowikens <kuban96166@gmail.com>
2025-05-17 22:25:08 +0200

    feature: add snake-like constant movement, input changes direction - PGryc commit - I'm just the messenger

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,29 +2,33 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
+const DIRECTION = Vector2(0,0)
+var next_direction = Vector2(0,0)
+var frame = 0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
-	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var vertical_direction := Input.get_axis("ui_up", "ui_down")
-	if vertical_direction:
-		velocity.y = vertical_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	var horizontal_input := Input.get_axis("ui_left", "ui_right")
+	var vertical_input := Input.get_axis("ui_up", "ui_down")
+
+	if DIRECTION.x == 0:
+		if horizontal_input > 0:
+			next_direction = Vector2(1,0)
+		if horizontal_input < 0:
+			next_direction = Vector2(-1,0)
+
+	if DIRECTION.y == 0:
+		if vertical_input > 0:
+			next_direction = Vector2(0,1)
+		if vertical_input < 0:
+			next_direction = Vector2(0,-1)
+
+	if frame % 10 == 0:
+		velocity.x = next_direction.x * SPEED
+		velocity.y = next_direction.y * SPEED
+
 	move_and_slide()
+	frame += 1
+	##position = bounds.wrap_vector(new_pos)
