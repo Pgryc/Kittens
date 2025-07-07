3dc56ba2291ac7168ad322908f1659342ca2a353
pgryc <czytopawelgryc@gmail.com>
2025-07-07 21:53:07 +0200

    feat: added calling <add_kitten> on collision of mother with a spawned one (picking kittens up feature)

--- scripts/spawned_kitten.gd
+++ scripts/spawned_kitten.gd
@@ -4,4 +4,8 @@ extends Area2D
 	
 	
 func _on_body_entered(body: Node2D) -> void:
+	if body.get_class() == "CharacterBody2D" and body.has_method('add_kitten'):
+		body.add_kitten()
+  #TODO: this will immediately delete node spawned on child kitten without handling it in any way
+	self.call_deferred('free')
 	print("+1")
